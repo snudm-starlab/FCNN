dc216c03f558d0888d91d26a7d54356b2d9bb233
ant6si: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 <ant6si@snu.ac.kr>
2023-07-28 06:41:24 +0000

    update analysis

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ author baiyu
 """
 
 import os
+from re import I
 import sys
 import argparse
 import time
@@ -15,6 +16,7 @@ from tqdm import tqdm
 import copy
 
 import numpy as np
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -24,147 +26,58 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from models.fourier import *
+from models.fourier2 import *
 from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
 
-def train(epoch):
-
-    start = time.time()
-    net.train()
-    for batch_index, (images, labels) in tqdm(enumerate(cifar100_training_loader)):
-
-        if args.gpu:
-            labels = labels.cuda()
-            images = images.cuda()
-
-        optimizer.zero_grad()
-        outputs = net(images)
-        loss = loss_function(outputs, labels) + args.lam * reg(net, args.reg)
-        loss.backward()
-        optimizer.step()
-
-        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
-
-        last_layer = list(net.children())[-1]
-        for name, para in last_layer.named_parameters():
-            if 'weight' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
-            if 'bias' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
-        """
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
-        """
-        #update training loss for each iteration
-        writer.add_scalar('Train/loss', loss.item(), n_iter)
-
-        if epoch <= args.warm:
-            warmup_scheduler.step()
-
-    for name, param in net.named_parameters():
-        layer, attr = os.path.splitext(name)
-        attr = attr[1:]
-        writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
-
-    finish = time.time()
-
-    # print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
-
 @torch.no_grad()
-def eval_training(epoch=0, tb=True, thres=1e-2):
-    net2 = copy.deepcopy(net)
-    start = time.time()
-    net2.eval()
-    
-    # Remove parameters below threshold
-    zero_out(net2, thres)
-    num_params = count_params(net2, thres)
-
-    test_loss = 0.0 # cost function error
-    correct = 0.0
-
-    for (images, labels) in cifar100_test_loader:
-
+def _eval():
+    tot_mse = 0; count = 0
+    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
         if args.gpu:
-            images = images.cuda()
             labels = labels.cuda()
+            images = images.cuda()
 
-        outputs = net2(images)
-        loss = loss_function(outputs, labels)
-
-        test_loss += loss.item()
-        _, preds = outputs.max(1)
-        correct += preds.eq(labels).sum()
-
-    finish = time.time()
-    # print('Evaluating Network.....')
-    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s, Params: {:.2f}M'.format(
-        epoch,
-        test_loss / len(cifar100_test_loader.dataset),
-        correct.float() / len(cifar100_test_loader.dataset),
-        finish - start,
-        num_params/1e6
-    ))
-    print()
-
-    #add informations to tensorboard
-    if tb:
-        writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
-        writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
-
-    return correct.float() / len(cifar100_test_loader.dataset)
+        inputs = prev_layers(images)
+        _target = target_layer(inputs)
+        _out = fourier_layer(inputs)
+        _diff = _target - _out
+        _mse = (_diff * _diff).mean()
+        tot_mse += _mse
+        count += 1
+    return tot_mse / count
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('-net', type=str, default='resnet34', help='net type')
+    parser.add_argument('-seed', type=int, default=0, help='random seed for reproducibility')
+    parser.add_argument('-gpu', action='store_true', default=True, help='use gpu or not')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
-    parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
-    parser.add_argument('-reg', type=str, required=True, help='l1 or l2 reg')
-    parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
-    parser.add_argument('-lam', type=float, default=0.0001, help='regularization coefficient')
-    parser.add_argument('-thres', type=float, default=1e-2, help='pruning threshold')
-    parser.add_argument('-load', action='store_true', default=False, help='load pretrained or not')
-    parser.add_argument('-resume', action='store_true', default=False, help='resume training')
+    parser.add_argument('-lr', type=float, default=5e-6, help='initial learning rate')
+    parser.add_argument('-epoch', type=int, default=30, help='epochs to train')
+    parser.add_argument('-load', action='store_true', default=True, help='load pretrained or not')
     args = parser.parse_args()
 
     ###################################
     print("="*80)
     print(args)
     print("="*80)
+
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(args.seed)
+    random.seed(args.seed)
     ###################################
 
     net = get_network(args)
-
-    ####################### Load & Convert #####################
+    ############### Load weights and get target layers  ############################
     if args.load:
         weight_path = 'checkpoint/resnet34/test.pth'
-        net.load_state_dict(torch.load(weight_path))
-    
-    _bs = [1,3,4,6,3]
-    for _l in [2, 3, 4, 5]:
-    # for _l in [2,3,4,5]:
-        b_list = np.arange(_bs[_l-1])
-        for _b in b_list:
-            for _n in [0, 3]:
-                # Index of convolution in the Sequential Function
-                # if _l==4 and _b == 0 and _n ==0:
-                #     continue
-                net = convert(net, _l, _b, _n)
-    if args.gpu:
-        net = net.cuda()
-    net2 = copy.deepcopy(net)
-    net2.eval()
-    num_params = count_params(net2, args.thres)
-    print(f"Converting ends, # params: {num_params/1e6:.2f}M")
+        net.load_state_dict(torch.load(weight_path)) 
+    # print(net)
     ############################################################
 
     #data preprocessing:
@@ -183,90 +96,85 @@ if __name__ == '__main__':
         batch_size=args.b,
         shuffle=True
     )
+    ############################# Prepare training #####################################
 
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
-    iter_per_epoch = len(cifar100_training_loader)
-    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
+    prev_layers = nn.Sequential(
+            net.conv1,
+            )
+    target_layer = nn.Sequential(
+            net.conv2_x[0].residual_function[0], # Conv2d
+            # net.conv2_x[0].residual_function[1], # BatchNorm
+            # net.conv2_x[0].residual_function[2], # ReLU
+            )
+    fourier_layer = nn.Sequential(
+            FConv2d(32, 64, 1, 1),
+            # copy.deepcopy(net.conv2_x[0].residual_function[1]), # BatchNorm
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            FConv2d(32, 64, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            FConv2d(32, 64, 1, 1),
+            # nn.BatchNorm2d(64),
+            )
+    print("* Target layer: ", target_layer)
+    print("* Fourier layer: ", fourier_layer)
 
-    if args.resume:
-        recent_folder = most_recent_folder(os.path.join(settings.CHECKPOINT_PATH, args.net), fmt=settings.DATE_FORMAT)
-        if not recent_folder:
-            raise Exception('no recent folder were found')
+    # print(target_layer)
+    # fourier_layer = FConv2d(32, 64, 1, 1)
 
-        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder)
+    prev_layers.eval()
+    target_layer.eval()
+    fourier_layer.train()
+    prev_layers.cuda()
+    target_layer.cuda()
+    fourier_layer.cuda()
 
-    else:
-        checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
+    # optimizer = optim.SGD(fourier_layer.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+    # optimizer = optim.SGD([fourier_layer.weight, target_layer.weight], lr=args.lr)
+    optimizer = optim.SGD(fourier_layer.parameters(), lr=args.lr)
+    # test_input = torch.randn(1, 3,32,32)
+    # _feature = prev_layers(test_input)
+    # target_feature = target_layer(_feature)
+    # _feature = fourier_layer(_feature)
+    # print("target feature: ", target_feature.shape)
+    # print("generated feature: ", _feature.shape) 
+    #####################################################################################
+    
+    for epoch in range(1, args.epoch + 1):
+        # print(f"Epoch #{epoch:2d} |", end = '')
+        train_mse = 0; _count = 0; orig_output = 0; fourier_output = 0
+        for batch_index, (images, labels) in tqdm(enumerate(cifar100_training_loader)):
+            if args.gpu:
+                labels = labels.cuda()
+                images = images.cuda()
+            inputs = prev_layers(images).detach()
+            _target = target_layer(inputs).detach()
+            _out = fourier_layer(inputs)
+            # print("_target: ", (_target*_target).mean())
+            _diff = _target - _out
+            loss = (_diff * _diff).mean()
+            # loss = torch.sqrt((_diff * _diff).mean())
+            train_mse += loss
+            _count += 1
+            orig_output += (_target * _target).mean().detach()
+            fourier_output += (_out * _out).mean().detach()
+            # print(f"* MSE: {loss:.4f}")
+            loss.backward()
+            optimizer.step()
+        test_mse = _eval()
+        print(f"{epoch:3d} | orig_output: {orig_output/_count:.5f} | fourier_output: {fourier_output/_count:.5f}",
+              f"| train_mse: {train_mse/_count:.5f} | test_mse = {test_mse:.5f})")
 
-    #use tensorboard
-    if not os.path.exists(settings.LOG_DIR):
-        os.mkdir(settings.LOG_DIR)
-
-    #since tensorboard can't overwrite old values
-    #so the only way is to create a new tensorboard log
-    writer = SummaryWriter(log_dir=os.path.join(
-            settings.LOG_DIR, args.net, settings.TIME_NOW))
-    input_tensor = torch.Tensor(1, 3, 32, 32)
-    if args.gpu:
-        input_tensor = input_tensor.cuda()
-    writer.add_graph(net, input_tensor)
-
-    #create checkpoint folder to save model
-    if not os.path.exists(checkpoint_path):
-        os.makedirs(checkpoint_path)
-    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
-
-    best_acc = 0.0
-    if args.resume:
-        best_weights = best_acc_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
-        if best_weights:
-            weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, best_weights)
-            print('found best acc weights file:{}'.format(weights_path))
-            print('load best training file to test acc...')
-            net.load_state_dict(torch.load(weights_path))
-            best_acc = eval_training(tb=False)
-            print('best acc is {:0.2f}'.format(best_acc))
-
-        recent_weights_file = most_recent_weights(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
-        if not recent_weights_file:
-            raise Exception('no recent weights file were found')
-        weights_path = os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder, recent_weights_file)
-        print('loading weights file {} to resume training.....'.format(weights_path))
-        net.load_state_dict(torch.load(weights_path))
-
-        resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
-
-
-    for epoch in range(1, settings.EPOCH + 1):
-        if epoch > args.warm:
-            train_scheduler.step(epoch)
-
-        if args.resume:
-            if epoch <= resume_epoch:
-                continue
-
-        train(epoch)
-        acc = eval_training(epoch, thres=args.thres)
+    # best_acc = 0.0
+    """
+    for epoch in range(1, args.epoch + 1):
+        pass
+        # train(epoch)
+        # acc = eval_training(epoch, thres=args.thres)
 
         #start to save best performance model after learning rate decay to 0.01
-        if epoch > settings.MILESTONES[1] and best_acc < acc:
-            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='best')
-            print('saving weights file to {}'.format(weights_path))
-            torch.save(net.state_dict(), weights_path)
-            best_acc = acc
-            continue
+    """
 
-        if not epoch % settings.SAVE_EPOCH:
-            weights_path = checkpoint_path.format(net=args.net, epoch=epoch, type='regular')
-            print('saving weights file to {}'.format(weights_path))
-            torch.save(net.state_dict(), weights_path)
 
-    writer.close()
 
-    ###################################
-    print("="*80)
-    print(args)
-    print("="*80)
-    ###################################
