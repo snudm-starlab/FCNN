47852199dac49dca72802a9ff8ec8305805f210a
ant6si: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 <ant6si@snu.ac.kr>
2023-07-31 02:49:52 +0000

    fft -> rfft

--- models/fourier.py
+++ models/fourier.py
@@ -35,8 +35,8 @@ class FConv2d(torch.nn.Module):
         _pad = self.k//2
         l_pad = self.l + _pad
         sr = (self.n * self.cin) // (self.cout) # shrink ratio
-        x_hat = fftn(x, s=[self.cin, l_pad, l_pad]) # input: [b, cin, l, l]
-        w_hat = fftn(self.weight, s=[self.cin, l_pad, l_pad])
+        x_hat = rfftn(x, s=[self.cin, l_pad, l_pad]) # input: [b, cin, l, l]
+        w_hat = rfftn(self.weight, s=[self.cin, l_pad, l_pad])
         # print("x_hat.shape: ", x_hat.shape, "w_hat.shape: ", w_hat.shape)
         # freq_out = torch.einsum('bchw,nchw->bnchw', x_hat, w_hat)
         # out = irfftn(freq_out, dim=[2,3,4]).real # b,n,c,h,w
@@ -47,7 +47,7 @@ class FConv2d(torch.nn.Module):
         # print("*** freq_out: ", freq_out.shape)
         # freq_out = freq_out.view(_b, _n, sr, -1, _h, _w).sum(dim=2)
         # out = ifftn(freq_out, dim=[2,3,4]).real # b,n,c,h,w
-        out = ifftn(freq_out, s=[self.cin, l_pad, l_pad]).real[:,:,:,_pad:, _pad:] # b,n,c,h,w
+        out = irfftn(freq_out, s=[self.cin, l_pad, l_pad]).real[:,:,:,_pad:, _pad:] # b,n,c,h,w
 
         # channel shuffling
         _inds = torch.arange(self.cin)[torch.arange(self.cin)%(sr)==0].to(x.device)
