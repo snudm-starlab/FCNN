cd7e2a508ab633571145916f4e4af7faadbcf1d9
ant6si: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 <ant6si@snu.ac.kr>
2023-08-11 15:58:48 +0000

    edit fourier.py

--- models/fourier.py
+++ models/fourier.py
@@ -17,8 +17,10 @@ class FConv2d(torch.nn.Module):
         self.s = stride
         self.kappa = kappa
         
+        
         kernel_size=3
         self.k = kernel_size
+        self.wn = self.get_wn().cuda()
         """
         if kernel_size is not None:
             self.k = kernel_size
@@ -29,23 +31,32 @@ class FConv2d(torch.nn.Module):
                 self.k = self.l//2 + 1
         """
         _range = torch.sqrt(torch.tensor(self.kappa/(self.cin * self.k * self.k)))
+        _pad = self.k//2
+        l_pad = self.l + _pad
+        _w = (torch.rand(self.n, self.cin//self.kappa , self.k, self.k) - 0.5) * 2 * _range
+        w_hat = fftn(_w, s=[self.cin, l_pad, l_pad])
+        self.weight = nn.Parameter(w_hat)        
+        """
         self.weight = nn.Parameter(
                 (torch.rand(self.n, self.cin//self.kappa , self.k, self.k) - 0.5) * 2 * _range
-                )
+                )        
+        """
 
     def forward(self, x):
         _pad = self.k//2
         l_pad = self.l + _pad
-        sr = (self.n * self.cin) // (self.cout) # shrink ratio
-        x_hat = rfftn(x, s=[self.cin, l_pad, l_pad]) # input: [b, cin, l, l]
-        w_hat = rfftn(self.weight, s=[self.cin, l_pad, l_pad])
+        x_hat = fftn(x, s=[self.cin, l_pad, l_pad]) # input: [b, cin, l, l]
+        # w_hat = fftn(self.weight, s=[self.cin, l_pad, l_pad])
+        w_hat = self.weight
         freq_out = torch.einsum('bchw,nchw->bnchw', x_hat, w_hat)
-        out = irfftn(freq_out, s=[self.cin, l_pad, l_pad]).real[:,:,:,_pad:, _pad:] # b,n,c,h,w
+        out = ifftn(freq_out, s=[l_pad, l_pad]) # .real[:,:,:,_pad:, _pad:] # b,n,c,h,w
+        # print("****** Type: ", out.dtype, self.wn.dtype)
+        out = torch.einsum("cd, bnchw->bndhw", self.wn, out)
+        out = out.real[:,:,:,_pad:, _pad:] # b,n,c,h,w
 
         # channel shuffling
-        _inds = torch.arange(self.cin)[torch.arange(self.cin)%(sr)==0].to(x.device)
-        out = torch.index_select(out, 2, _inds) # [b, n, c//n, h, w] # removing channels
-        out = out.transpose(1,2).contiguous()
+        # out = torch.index_select(out, 2, self._inds) # [b, n, c//n, h, w] # removing channels
+        out = out.transpose(1,2).contiguous() # shuffling
         out = out.view(out.shape[0], -1, out.shape[3], out.shape[4]) # [b,c,h,w]
  
         if self.s==2:
@@ -54,6 +65,16 @@ class FConv2d(torch.nn.Module):
             out = torch.index_select(out, 3, _inds)
         
         return out
+
+    def get_wn(self,):
+        _v = torch.arange(self.cin).view(self.cin,-1)
+        _sr = (self.n * self.cin) // (self.cout) # shrink ratio
+        _inds = torch.arange(self.cin)[torch.arange(self.cin)%(_sr)==0]
+        _M = _v @ _v[_inds,:].T
+        _w = torch.exp(torch.complex(real=torch.tensor([0.]), 
+                                     imag=torch.tensor([(2*torch.pi)/(self.cin)])))
+        wn = torch.pow(_w, _M) / self.cin # shape: [cin, cin//sr]
+        return wn
 
 
 
