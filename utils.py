6cdada557cd805d89671067fd41c46ec77b6a691
ant6si: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 <ant6si@snu.ac.kr>
2023-10-22 18:06:24 +0900

    update utils.py

--- utils.py
+++ utils.py
@@ -19,155 +19,21 @@ from torch.utils.data import DataLoader
 def get_network(args):
     """ return given network
     """
-
-    if args.net == 'vgg16':
-        from models.vgg import vgg16_bn
-        net = vgg16_bn()
-    elif args.net == 'vgg13':
-        from models.vgg import vgg13_bn
-        net = vgg13_bn()
-    elif args.net == 'vgg11':
-        from models.vgg import vgg11_bn
-        net = vgg11_bn()
-    elif args.net == 'vgg19':
-        from models.vgg import vgg19_bn
-        net = vgg19_bn()
-    elif args.net == 'densenet121':
-        from models.densenet import densenet121
-        net = densenet121()
-    elif args.net == 'densenet161':
-        from models.densenet import densenet161
-        net = densenet161()
-    elif args.net == 'densenet169':
-        from models.densenet import densenet169
-        net = densenet169()
-    elif args.net == 'densenet201':
-        from models.densenet import densenet201
-        net = densenet201()
-    elif args.net == 'googlenet':
-        from models.googlenet import googlenet
-        net = googlenet()
-    elif args.net == 'inceptionv3':
-        from models.inceptionv3 import inceptionv3
-        net = inceptionv3()
-    elif args.net == 'inceptionv4':
-        from models.inceptionv4 import inceptionv4
-        net = inceptionv4()
-    elif args.net == 'inceptionresnetv2':
-        from models.inceptionv4 import inception_resnet_v2
-        net = inception_resnet_v2()
-    elif args.net == 'xception':
-        from models.xception import xception
-        net = xception()
-    elif args.net == 'resnet18':
-        from models.resnet import resnet18
-        net = resnet18()
-    elif args.net == 'resnet34':
+    if args.net == 'resnet34':
         from models.resnet import resnet34
         net = resnet34()
-    elif args.net == 'fresnet34':
-        ######## Fourier Resnet ###########
-        from models.fourier_resnet import fresnet34
-        net = fresnet34(nu=args.nu, kappa=args.kappa)
-        ###################################
-    elif args.net == 'fresnetv2_34':
-        ######## Fourier Resnet ###########
-        from models.fresnet_v2 import fresnetv2_34
-        net = fresnetv2_34(nu=args.nu, kappa=args.kappa)
-        # print(net)
-        ###################################
+    
     elif args.net == 'conv3dresnet34':
-        ######## Fourier conv3d Resnet ###########
         from models.conv3d_resnet import conv3dresnet34
         net = conv3dresnet34(nu=args.nu, kappa=args.kappa)
-        ###################################
-    elif args.net == 'resnet50':
-        from models.resnet import resnet50
-        net = resnet50()
-    elif args.net == 'resnet101':
-        from models.resnet import resnet101
-        net = resnet101()
-    elif args.net == 'resnet152':
-        from models.resnet import resnet152
-        net = resnet152()
-    elif args.net == 'preactresnet18':
-        from models.preactresnet import preactresnet18
-        net = preactresnet18()
-    elif args.net == 'preactresnet34':
-        from models.preactresnet import preactresnet34
-        net = preactresnet34()
-    elif args.net == 'preactresnet50':
-        from models.preactresnet import preactresnet50
-        net = preactresnet50()
-    elif args.net == 'preactresnet101':
-        from models.preactresnet import preactresnet101
-        net = preactresnet101()
-    elif args.net == 'preactresnet152':
-        from models.preactresnet import preactresnet152
-        net = preactresnet152()
-    elif args.net == 'resnext50':
-        from models.resnext import resnext50
-        net = resnext50()
-    elif args.net == 'resnext101':
-        from models.resnext import resnext101
-        net = resnext101()
-    elif args.net == 'resnext152':
-        from models.resnext import resnext152
-        net = resnext152()
-    elif args.net == 'shufflenet':
-        from models.shufflenet import shufflenet
-        net = shufflenet()
-    elif args.net == 'shufflenetv2':
-        from models.shufflenetv2 import shufflenetv2
-        net = shufflenetv2()
-    elif args.net == 'squeezenet':
-        from models.squeezenet import squeezenet
-        net = squeezenet()
-    elif args.net == 'mobilenet':
-        from models.mobilenet import mobilenet
-        net = mobilenet()
-    elif args.net == 'mobilenetv2':
-        from models.mobilenetv2 import mobilenetv2
-        net = mobilenetv2()
-    elif args.net == 'nasnet':
-        from models.nasnet import nasnet
-        net = nasnet()
-    elif args.net == 'attention56':
-        from models.attention import attention56
-        net = attention56()
-    elif args.net == 'attention92':
-        from models.attention import attention92
-        net = attention92()
-    elif args.net == 'seresnet18':
-        from models.senet import seresnet18
-        net = seresnet18()
-    elif args.net == 'seresnet34':
-        from models.senet import seresnet34
-        net = seresnet34()
-    elif args.net == 'seresnet50':
-        from models.senet import seresnet50
-        net = seresnet50()
-    elif args.net == 'seresnet101':
-        from models.senet import seresnet101
-        net = seresnet101()
-    elif args.net == 'seresnet152':
-        from models.senet import seresnet152
-        net = seresnet152()
-    elif args.net == 'wideresnet':
-        from models.wideresidual import wideresnet
-        net = wideresnet()
-    elif args.net == 'stochasticdepth18':
-        from models.stochasticdepth import stochastic_depth_resnet18
-        net = stochastic_depth_resnet18()
-    elif args.net == 'stochasticdepth34':
-        from models.stochasticdepth import stochastic_depth_resnet34
-        net = stochastic_depth_resnet34()
-    elif args.net == 'stochasticdepth50':
-        from models.stochasticdepth import stochastic_depth_resnet50
-        net = stochastic_depth_resnet50()
-    elif args.net == 'stochasticdepth101':
-        from models.stochasticdepth import stochastic_depth_resnet101
-        net = stochastic_depth_resnet101()
+
+    elif args.net == 'dscresnet34':
+        from models.dsc_resnet import dscresnet34
+        net = dscresnet34()
+
+    elif args.net == 'gcresnet34':
+        from models.gc_resnet import gcresnet34
+        net = gcresnet34(nu=args.nu)
 
     else:
         print('the network name you have entered is not supported yet')
