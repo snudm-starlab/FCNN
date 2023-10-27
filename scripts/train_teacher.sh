69e2d4d2e92cc659f69ce0376afb71f8e0bb75f8
DM Lab - Starlab <68460141+snudm-starlab@users.noreply.github.com>
2023-10-28 03:19:29 +0900

    Update train_teacher.sh

--- scripts/train_teacher.sh
+++ scripts/train_teacher.sh
@@ -3,5 +3,4 @@ NET=resnet34
 RHO=6
 NU=2
 cd src/
-CUDA_VISIBLE_DEVICES=$GPU python train.py -net $NET -gpu -rho $RHO -nu $NU \
-                       -alpha 0.9 -tau 10
+CUDA_VISIBLE_DEVICES=$GPU python train.py -net $NET -gpu -rho $RHO -nu $NU 
