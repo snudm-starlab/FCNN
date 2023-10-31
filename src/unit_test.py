70f4fbfb8be45e36d9dcda8aa3907724841565aa
ant6si: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 <ant6si@snu.ac.kr>
2023-10-31 20:28:03 +0900

    add unittest

---
+++ src/unit_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class FCNNTest(unittest.TestCase):
+    def test_one_plus_two(self):
+        self.assertEqual(1+2,3)
+
+
+    def test_one_plus_two_3(self):
+        self.assertEqual(2,2)
